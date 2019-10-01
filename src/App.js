3354a0951c7f1d6d623facda1b1c56a31c62f44c
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-10-01 15:28:26 -0600

    styled with emotion css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import SearchPage from "./pages/searchPages";
-import "./App.css";
+import { Global, css } from "@emotion/core";
+import normalize from "normalize.css";
+import { ThemeProvider } from "emotion-theming";
+import theme from "./components/theme";
+import SearchPage from "./pages/searchPage";
 import BookDetailPage from "./pages/bookDetailPage";
+import "./App.css";
 
-const NoMatchRoute = () => <div>404 Page</div>;
+const NoMatchRoute = () => <div>404 Page does not exist</div>;
+
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={SearchPage} />
-        <Route component={NoMatchRoute} />
-        <Route path="/book/:bookId" exact component={BookDetailPage} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          ${normalize}
+          body {
+            background-color: #fafafa;
+          }
+        `}
+      />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={SearchPage} />
+          <Route path="/book/:bookId" exact component={BookDetailPage} />
+          <Route component={NoMatchRoute} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- src/components/booksSearchForm.js
+++ src/components/booksSearchForm.js
@@ -1,4 +1,35 @@
 import React from "react";
+import styled from "@emotion/styled";
+import ErrorText from "./errorText";
+
+const Input = styled.input`
+  outline: 0;
+  padding: 0.6rem 1rem;
+  border: 1px solid rgba(34, 36, 38, 0.15);
+  border-radius: 3px;
+  min-width: 280px;
+  &:focus,
+  &:active {
+    border-color: #85b7d9;
+  }
+  @media (max-width: 778px) {
+    margin-top: 10px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #2185d0;
+  color: #fff;
+  text-shadow: none;
+  padding: 0.6rem 1.5rem;
+  margin-left: 15px;
+  border-radius: 3px;
+  cursor: pointer;
+  @media (max-width: 778px) {
+    margin-left: 0;
+    margin-top: 10px;
+  }
+`;
 
 const BookSearchForm = ({
   onSubmitHandler,
@@ -8,21 +39,16 @@ const BookSearchForm = ({
 }) => {
   return (
     <form onSubmit={onSubmitHandler}>
-      <label>
-        <span>Search for books</span>
-        <input
-          type="search"
-          placeholder="enter book name"
-          value={searchTerm}
-          onChange={onInputChange}
-          required
-        />
-        <button type="submit">Search</button>
-      </label>
+      <Input
+        type="search"
+        placeholder="Search for books"
+        value={searchTerm}
+        onChange={onInputChange}
+        required
+      />
+      <Button type="submit">Search</Button>
       {error && (
-        <div style={{ color: `red` }}>
-          some error occurred while searching book
-        </div>
+        <ErrorText>Some error occurred while searching for book</ErrorText>
       )}
     </form>
   );
